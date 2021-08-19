ceef4dfcadd77ec9a187fa91e950fe66ce0343a1
Mahmut Can Kurt <is.mahmutcankurt@gmail.com>
2021-08-19 23:31:08 +0300

    linked list reverse a list

---
+++ LinkedLists/linked-list-reverse-a-list.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
