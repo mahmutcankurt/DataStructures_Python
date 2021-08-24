28f25e2a88178127a2157ad6e4905d2967a0e3f3
Mahmut Can Kurt <is.mahmutcankurt@gmail.com>
2021-08-24 22:11:26 +0300

    stack check balanced brackets

---
+++ Stack/6-stack-balanced-brackets-checking.py
@@ -0,0 +1,39 @@
+def balanced(expr):
+    stack = []
+
+    for char in expr:
+        if(char in ["(", "{", "["]):
+            stack.append(char)
+        
+        else:
+            if(not stack):
+                return False
+            
+            current_char = stack.pop()
+
+            if(current_char == "("):
+                if(char != ")"):
+                    return False
+            
+            if(current_char == "{"):
+                if(char != "}"):
+                    return False
+            
+            if(current_char == "{"):
+                if(char != "}"):
+                    return False
+    
+    if stack:
+        return False
+    return True
+
+if __name__ == "__main__":
+    expr = "{()}[]"
+
+    if balanced(expr):
+        print("Balanced")
+    else:
+        print("Not Balanced")
+
+        
+            
