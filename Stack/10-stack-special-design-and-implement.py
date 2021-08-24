1642f7280a406ed446f1b56bf03c689104cbbef5
Mahmut Can Kurt <is.mahmutcankurt@gmail.com>
2021-08-25 00:17:29 +0300

    stack special design and implement

--- Stack/10-stack-special-design-and-implement.py
+++ Stack/10-stack-special-design-and-implement.py
@@ -23,3 +23,51 @@ class Stack:
         else:
             self.top += 1
             self.array.append(data)
+
+    def pop(self):
+        if(self.isEmpty()):
+            print("Stack Underflow")
+            return
+        else:
+            self.top -= 1
+            return(self.array.pop())
+
+class SpecialStack(Stack):
+    def __init__(self):
+        super().__init__()
+        self.Min = Stack()
+
+    def push(self, x):
+        if(self.isEmpty):
+            super().push(x)
+            self.Min.push(x)
+
+        else:
+            super().push(x)
+            y = self.Min.pop()
+            self.Min.push(y)
+
+            if(x <= y):
+                self.Min.push(x)
+            else: 
+                self.Min.push(y)
+    
+    def pop(self):
+        x = super().pop()
+        self.Min.pop()
+        return x
+
+    def getMin(self):
+        x = self.Min.pop()
+        self.Min.push(x)
+        return x
+
+if __name__ == "__main__":
+    s = SpecialStack()
+
+    s.push(10)
+    s.push(20)
+    s.push(30)
+    print(s.getMin())
+    s.push(5)
+    print(s.getMin())
